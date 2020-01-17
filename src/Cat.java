893eb3d8e49588bec2aaa793341955569c234ec1
Max <sobolmax1984@mail.com>
2020-01-17 23:54:08 +0200

    Animals say

---
+++ src/Cat.java
@@ -0,0 +1,7 @@
+public class Cat extends Pet {
+
+    @Override
+    public void voice() {
+        System.out.println("Я кіт- Мяууу-Мяууу");
+    }
+}
---
+++ src/Cow.java
@@ -0,0 +1,8 @@
+public class Cow extends Pet {
+
+
+    @Override
+    public void voice() {
+        System.out.println("Я корова- Мууу-Мууу");
+    }
+}
