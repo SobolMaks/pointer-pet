edf374a629093d94f298a9d7b7a5ffd6751db5ce
Max <sobolmax1984@mail.com>
2020-01-27 16:14:28 +0200

    Pet

---
+++ Pet/src/Cow.java
@@ -0,0 +1,7 @@
+public class Cow extends Pet {
+    @Override
+    public void voice() {
+        super.voice();
+        System.out.println("Я корова- Мууу-Мууу ");
+    }
+}
---
+++ Pet/src/Dog.java
@@ -0,0 +1,7 @@
+public class Dog extends Pet {
+    @Override
+    public void voice() {
+        super.voice();
+        System.out.println("Я пес- Гаууу-Гаууу ");
+    }
+}
